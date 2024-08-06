40ac1b4614742cc50616e4eb355137d31a3bc13a
yejine2 <yejine2@naver.com>
2024-08-06 15:48:21 +0900

    chore: storybook setup

---
+++ packages/react/components/layout/src/block.css.ts
@@ -0,0 +1,8 @@
+import { vars } from "@ooz/themes";
+import { style } from "@vanilla-extract/css";
+
+export const blockStyle = style({
+  width: "100px",
+  height: "100px",
+  backgroundColor: vars.colors.$scale.blue[300],
+});
