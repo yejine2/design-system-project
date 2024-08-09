d2a2db810e6760ef8bd43888685ec1469af68328
yejine2 <yejine2@naver.com>
2024-08-10 00:29:12 +0900

    feat: 버튼 컴포넌트 패키지 추가

---
+++ packages/react/components/button/src/Button.tsx
@@ -0,0 +1,87 @@
+import * as React from "react";
+import { ButtonProps } from "./types";
+import { clsx } from "clsx";
+import { assignInlineVars } from "@vanilla-extract/dynamic";
+import {
+  activeColorVariant,
+  buttonStyle,
+  enableColorVariant,
+  hoverColorVariant,
+  spanStyle,
+  spinnerStyle,
+} from "./style.css";
+import { vars } from "@ooz/themes";
+
+const Button = (props: ButtonProps, ref: React.Ref<HTMLButtonElement>) => {
+  const {
+    variant = "solid",
+    size = "md",
+    color = "gray",
+
+    leftIcon,
+    rightIcon,
+    isLoading,
+    isDisabled = false,
+    children,
+    onKeyDown,
+    style,
+  } = props;
+
+  const enableColor = vars.colors.$scale[color][500];
+  const hoverColor =
+    variant === "solid"
+      ? vars.colors.$scale[color][600]
+      : vars.colors.$scale[color][50];
+  const activeColor =
+    variant === "solid"
+      ? vars.colors.$scale[color][700]
+      : vars.colors.$scale[color][100];
+
+  const disabled = isDisabled || isLoading;
+
+  // button a11y
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLButtonElement>) => {
+    onKeyDown?.(event);
+
+    if (event.key === "Enter" || event.key === "13") {
+      event.preventDefault();
+      event.currentTarget.click();
+    }
+  };
+
+  return (
+    <button
+      {...props}
+      ref={ref}
+      onKeyDown={handleKeyDown}
+      onClick={() => {
+        console.log("test");
+      }}
+      role="button"
+      className={clsx([
+        buttonStyle({
+          size,
+          variant,
+        }),
+      ])}
+      data-loading={isLoading}
+      disabled={disabled}
+      style={{
+        ...assignInlineVars({
+          [enableColorVariant]: enableColor,
+          [hoverColorVariant]: hoverColor,
+          [activeColorVariant]: activeColor,
+        }),
+        ...style,
+      }}
+    >
+      {isLoading && <div className={spinnerStyle({ size })} />}
+      {leftIcon && <span className={spanStyle({ size })}>{leftIcon}</span>}
+      <span>{children}</span>
+      {rightIcon && <span className={spanStyle({ size })}>{rightIcon}</span>}
+    </button>
+  );
+};
+
+const _Button = React.forwardRef(Button);
+export { _Button as Button };
---
+++ services/storybook/stories/ReactComponents/Layout/Button.stories.tsx
@@ -0,0 +1,37 @@
+import "@ooz/react-components-button/style.css";
+import { Button as _Button } from "@ooz/react-components-button";
+import { vars } from "@ooz/themes";
+
+export default {
+  title: "React Components/Button",
+  component: _Button,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    size: {
+      options: ["xs", "sm", "md", "lg"],
+      control: "select",
+    },
+    color: {
+      options: Object.keys(vars.colors.$scale),
+      control: "select",
+    },
+    variant: {
+      options: ["solid", "outline", "ghost"],
+      control: "select",
+    },
+  },
+};
+
+export const ButtonStory = {
+  args: {
+    size: "lg",
+    children: "Button",
+    variant: "outline",
+    isDisabled: false,
+    isLoading: false,
+    leftIcon: "😀",
+  },
+};
