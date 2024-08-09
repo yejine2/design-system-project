3a7088b3c48486cde52a10a90c8f7198118b3f86
yejine2 <yejine2@naver.com>
2024-08-09 19:47:58 +0900

    feat: 레이아웃 컴포넌트 - Grid

--- packages/react/components/layout/src/layout/types.ts
+++ packages/react/components/layout/src/layout/types.ts
@@ -21,3 +21,27 @@ export type FlexProps = {
   wrap?: CSSProperties["flexWrap"];
   gap?: number;
 } & BoxProps;
+
+export type GridProps = {
+  autoColumns?: CSSProperties["gridAutoColumns"];
+  autoFlow?: CSSProperties["gridAutoFlow"];
+  autoRows?: CSSProperties["gridAutoRows"];
+  column?: CSSProperties["gridColumn"];
+  columnGap?: CSSProperties["columnGap"];
+  gap?: CSSProperties["gap"];
+  row?: CSSProperties["gridRow"];
+  rowGap?: CSSProperties["rowGap"];
+  templateAreas?: CSSProperties["gridTemplateAreas"];
+  templateColumns?: CSSProperties["gridTemplateColumns"];
+  templateRows?: CSSProperties["gridTemplateRows"];
+} & BoxProps;
+
+export type GridItemProps = {
+  area?: CSSProperties["gridArea"];
+  colEnd?: CSSProperties["gridColumnEnd"];
+  colStart?: CSSProperties["gridColumnStart"];
+  colSpan?: CSSProperties["gridColumn"];
+  rowEnd?: CSSProperties["gridRowEnd"];
+  rowStart?: CSSProperties["gridRowStart"];
+  rowSpan?: CSSProperties["gridRow"];
+} & BoxProps;
