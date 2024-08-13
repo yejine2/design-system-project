c063fa79189bed285050caac2de30d20d74d6986
yejine2 <yejine2@naver.com>
2024-08-13 16:40:23 +0900

    feat: 리스트 컴포넌트 패키지 추가

--- packages/react/components/layout/src/index.ts
+++ packages/react/components/layout/src/index.ts
@@ -1,11 +1,24 @@
-export { Box, Divider, Flex, Grid, GridItem } from "./layout";
+export {
+  Box,
+  Divider,
+  Flex,
+  Grid,
+  GridItem,
+  OrderedList,
+  ListItem,
+  UnorderedList,
+  List,
+} from "./layout";
 export type {
   BoxProps,
   DividerProps,
   FlexProps,
   GridProps,
   GridItemProps,
+  OrderListProps,
+  ListItemProps,
+  UnorderedListProps,
+  ListProps,
 } from "./layout";
 
 export { Text, Heading } from "./typography";
-export type { TextProps, HeadingProps } from "./typography";
