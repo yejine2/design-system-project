55d39fe9352478205294aac7b2b5058dad244101
yejine2 <yejine2@naver.com>
2024-08-18 22:06:44 +0900

    feat: Text Input 응용 - Select Input

---
+++ packages/react/hooks/select/src/index.ts
@@ -0,0 +1,3 @@
+export type { UseSelectProps, UseSelectReturn } from "./types";
+
+export { useSelect } from "./useSelect";
---
+++ services/storybook/stories/ReactComponents/Select/Select.stories.tsx
@@ -0,0 +1,22 @@
+import "@ooz/react-components-select/style.css";
+import { Select } from "@ooz/react-components-select";
+import React from "react";
+
+import "@ooz/react-components-layout/style.css";
+
+export default {
+  title: "React Components/Select",
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export const SelectStory = {
+  render: () => (
+    <Select>
+      <option value="1">1</option>
+      <option value="2">2</option>
+    </Select>
+  ),
+};
