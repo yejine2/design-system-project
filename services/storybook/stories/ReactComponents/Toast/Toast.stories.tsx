e86d1fa05e9c57908dd16198808cb289b0e91e07
yejine2 <yejine2@naver.com>
2024-08-19 23:22:30 +0900

    feat: 토스트 컴포넌트 패키지 추가

---
+++ services/storybook/stories/ReactComponents/Toast/Toast.stories.tsx
@@ -0,0 +1,40 @@
+import "@ooz/react-components-button/style.css";
+import { Button } from "@ooz/react-components-button";
+import React from "react";
+
+import { ToastProvider, useToast } from "@ooz/react-components-toast";
+import "@ooz/react-components-toast/style.css";
+
+export default {
+  title: "React Components/Toast",
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+const Example = () => {
+  const { toast } = useToast();
+
+  return (
+    <Button
+      onClick={() =>
+        toast({
+          payload: {
+            message: "Hello, World!",
+          },
+        })
+      }
+    >
+      Show Toast
+    </Button>
+  );
+};
+
+export const ButtonStory = {
+  render: () => (
+    <ToastProvider>
+      <Example />
+    </ToastProvider>
+  ),
+};
