2af438ceeb3750f555f6026e76d8740438af7d8d
yejine2 <yejine2@naver.com>
2024-09-07 23:20:30 +0900

    chore: page builder 프로젝트 설정

---
+++ services/page-builder/src/pages/playground.tsx
@@ -0,0 +1,14 @@
+import { vars } from "@ooz/themes";
+import { CDN_BASE_URL } from "../constants";
+import { MobileFirstLayout } from "../components/view/MobileFirstLayout";
+
+const PlaygroundPage = () => {
+  return (
+    <MobileFirstLayout>
+      <h1>{vars.colors.$scale.gray[900]}</h1>
+      <img src={`${CDN_BASE_URL}/test/hero_mobile.webp`} alt="" />
+    </MobileFirstLayout>
+  );
+};
+
+export default PlaygroundPage;
