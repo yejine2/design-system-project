b7f2090dab48e0bdada426cf58508f969a489bd9
yejine2 <yejine2@naver.com>
2024-09-10 00:50:23 +0900

    refactor: 타이틀 및 이미지 슬라이더 통합 컴포넌트로 분리

---
+++ services/page-builder/src/components/view/slices/ImageSliderSection/index.tsx
@@ -0,0 +1,51 @@
+import { vars } from "@ooz/themes";
+import { Image, ImageSliderSlice } from "../ImageSlider";
+import { TextSlice } from "../Text";
+
+type Props = {
+  text: string;
+  images: Image[];
+  sliceStyle?: {
+    paddingX?: keyof typeof vars.box.spacing;
+    textColor?: string;
+    backgroundColor?: string;
+    imageItemWidth?: number;
+  };
+};
+
+export const ImageSliderSectionSlice = ({
+  text,
+  images,
+  sliceStyle,
+}: Props) => {
+  const {
+    paddingX = 8,
+    textColor = vars.colors.$static.light.color.black,
+    backgroundColor = vars.colors.$static.light.color.white,
+    imageItemWidth = 280,
+  } = sliceStyle ?? {};
+
+  return (
+    <>
+      <TextSlice
+        text={text}
+        sliceStyle={{
+          textSize: 20,
+          textColor,
+          backgroundColor,
+          textAlign: "left",
+          paddingX,
+          textWeight: 700,
+        }}
+      />
+      <ImageSliderSlice
+        images={images}
+        sliceStyle={{
+          paddingX,
+          imageItemWidth,
+          backgroundColor,
+        }}
+      />
+    </>
+  );
+};
